5c7305d8bc4281e5895935a11dae89e159570ea9
Brent Lemons <lembrent@amazon.com>
2020-04-11 00:38:41 -0500

    force json again

--- src/main/java/com/awsproserve/swim/ingest/SCDSMessageConsumer.java
+++ src/main/java/com/awsproserve/swim/ingest/SCDSMessageConsumer.java
@@ -94,9 +94,9 @@ public class SCDSMessageConsumer implements MessageListener {
 
 				logger.debug("raw message: " + msgTextObj);
 				
-				if (!streamJson) {
-					flightRecords.add(msgTextObj);
-				} else {
+//				if (!streamJson) {
+//					flightRecords.add(msgTextObj);
+//				} else {
 					try {
 						JAXBElement<MessageCollectionType> element = (JAXBElement<MessageCollectionType>) xmlToObject(msgTextObj);
 						List<AbstractMessageType> messages = ((MessageCollectionType)element.getValue()).getMessage();
@@ -117,7 +117,7 @@ public class SCDSMessageConsumer implements MessageListener {
 					} catch (JsonProcessingException e) {
 						logger.error(e.toString());
 					}
-				}
+//				}
 				
 				if (flightRecords.size() > 0) {
 					List<PutRecordsRequestEntry> kinesisRecords = new ArrayList<PutRecordsRequestEntry>();

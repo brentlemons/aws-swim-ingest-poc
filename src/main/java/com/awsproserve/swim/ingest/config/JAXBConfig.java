b4fdd61f7542512ab105f265b984ffcc3071f92f
Brent Lemons <lembrent@amazon.com>
2020-04-13 16:33:07 -0500

    try context path

--- src/main/java/com/awsproserve/swim/ingest/config/JAXBConfig.java
+++ src/main/java/com/awsproserve/swim/ingest/config/JAXBConfig.java
@@ -18,11 +18,12 @@ public class JAXBConfig {
 	public Jaxb2Marshaller marshaller() {
 		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 //		marshaller.setContextPath("aero.aixm.schema._5");
+//		marshaller.setContextPath("aero.aixm.schema._5");
 //		marshaller.setClassesToBeBound(
 ////				net.opengis.wfs.v_2_0.GetFeatureType.class,
 ////				aero.aixm.extensions.faa.fnse.ObjectFactory.class,
 //				aero.aixm.schema._5_1.ObjectFactory.class);
-		marshaller.setPackagesToScan("com.awsproserve.swim.ingest.entity.*");
+		marshaller.setContextPath("com.awsproserve.swim.ingest.entity");
 //		marshaller.setPackagesToScan("aero.*", "com.awsproserve.swim.ingest.entity.*");
 //		marshaller.setPackagesToScan("aero.*", "net.*", "org.*");
 //		marshaller.setPackagesToScan("aero.aixm.schema._5.*", "aero.aixm.schema._5_1.message.*", "aero.faa.nas._3");

13d1f80cf05e694c52333ee9e1a7022a8080aca4
Brent Lemons <lembrent@amazon.com>
2020-04-21 14:14:38 -0500

    add stdds xsds for xjc

--- src/main/java/com/awsproserve/swim/ingest/config/JAXBConfig.java
+++ src/main/java/com/awsproserve/swim/ingest/config/JAXBConfig.java
@@ -17,16 +17,7 @@ public class JAXBConfig {
 	@Bean
 	public Jaxb2Marshaller marshaller() {
 		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-//		marshaller.setContextPath("aero.aixm.schema._5");
-//		marshaller.setContextPath("aero.aixm.schema._5");
-//		marshaller.setClassesToBeBound(
-////				net.opengis.wfs.v_2_0.GetFeatureType.class,
-////				aero.aixm.extensions.faa.fnse.ObjectFactory.class,
-//				aero.aixm.schema._5_1.ObjectFactory.class);
-		marshaller.setContextPath("com.awsproserve.swim.ingest.entity");
-//		marshaller.setPackagesToScan("aero.*", "com.awsproserve.swim.ingest.entity.*");
-//		marshaller.setPackagesToScan("aero.*", "net.*", "org.*");
-//		marshaller.setPackagesToScan("aero.aixm.schema._5.*", "aero.aixm.schema._5_1.message.*", "aero.faa.nas._3");
+		marshaller.setPackagesToScan("aero.*", "us.*");
 		return marshaller;
 	}
 
